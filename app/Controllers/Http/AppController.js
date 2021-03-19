081878b7d9f8f4232291693301e11f04ade3b7d7
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-03-19 16:54:29 -0300

    Login and Register already

---
+++ app/Controllers/Http/AppController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class AppController {
+  async index({auth, response}) {
+    try {
+      //Get user informations
+      await auth.check()
+      const user = await auth.getUser()
+      return user
+
+    } catch(error){
+      response.send(error)
+    }
+  }
+
+}
+
+
+module.exports = AppController
---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class AuthController {
+  async register({ request }) {
+
+    // Request input from page
+    const data = request.only(["username", "email", "password"]);
+
+    // Save user in database
+    const user_data = await User.create(data);
+    return user_data
+  }
+
+  async login({ request, auth, response }) {
+
+    // Variables
+    const { email, password } = request.all()
+
+    try {
+
+      //Authenticate and Token generation
+      await auth.attempt(email, password)
+      const user = await User.findBy('email', email)
+      const acessToken = await auth.generate(user)
+
+      return response.json(acessToken)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = AuthController
