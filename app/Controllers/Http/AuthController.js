253777bc84c57367bfbda9ae0feb40bb8a530cc1
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-07-29 13:49:36 -0300

    update userInformation ready

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -46,24 +46,12 @@ class AuthController {
       await auth.check()
       const user = await auth.getUser()
 
-      // Get data from front-end
-      const data = request.only([
-        'adress',
-        'cep',
-        'phone',
-        'age',
-        'weight',
-        'height',
-        'chronicDisease'
-      ])
+      // Get data from front - end
+      const {username, password, imagePerfil} = request.all()
 
-      user.adress = data.adress
-      user.cep = data.ep
-      user.phoneNumber = data.phone
-      user.age = data.age
-      user.weight = data.weight
-      user.height = data.height
-      user.chronicDisease = data.chronicDisease
+      user.username = (username === null ? user.username : username)
+      user.password = (password === null ? user.password : password)
+      user.imagePerfil = (imagePerfil === null ? user.imagePerfil : imagePerfil)
 
       user.save()
 
@@ -87,3 +75,88 @@ class AuthController {
 }
 
 module.exports = AuthController
+
+// 'use strict'
+
+// const User = use('App/Models/User');
+
+// class AuthController {
+
+//   async register({ request }) {
+//     try {
+
+//       // Request input from page
+//       const data = request.only(["username", "email", "password", "imagePerfil"]);
+
+//       // Save user in database
+//       const user_data = await User.create(data);
+//       return user_data
+//     } catch (error) {
+//       console.log(error)
+
+//     }
+
+//   }
+
+//   async login({ request, auth, response }) {
+
+//     // Variables
+//     const { email, password } = request.all()
+
+//     try {
+
+//       //Authenticate and Token generation
+//       await auth.attempt(email, password)
+//       const user = await User.findBy('email', email)
+//       const acessToken = await auth.generate(user)
+
+//       return response.json(acessToken)
+
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   async update({ request, response, auth }) {
+
+//     try {
+//       // Check user and get his informations
+//       await auth.check()
+//       const user = await auth.getUser()
+
+//       // Get data from front-end
+//       const data = request.only([
+//         'username',
+//         'email',
+//         'password',
+//         'imagePerfil',
+//       ])
+
+//       user.username = data?.username
+//       user.email = data?.email
+//       user.password = data?.password
+//       user.imagePerfil = data?.imagePerfil
+
+//       user.save()
+
+//       return response.status(200).json("Dados atualizados!")
+//     } catch (error) {
+//       return console.log(error)
+//     }
+//   }
+
+//   async show({ auth, response }) {
+//     try {
+//       //Get user informations
+//       await auth.check()
+//       const user = await auth.getUser()
+//       return user
+
+//     } catch (error) {
+//       response.send(error)
+//     }
+//   }
+// }
+
+// module.exports = AuthController
+
