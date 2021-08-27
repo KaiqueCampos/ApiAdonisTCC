1fda612ace6df65be4d12b9b3d27fec1d3914269
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-08-27 11:25:15 -0300

    error saving image to database in binary format resolved, user table changed

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -10,7 +10,7 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.string('imagePerfil', 10000)
+      table.string('imagePerfil', 250000)
       table.timestamps()
     })
   }
